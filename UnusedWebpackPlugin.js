3faf8799b3f3e4f7a3a7b14d9f0a0192f76a1a5c
lishunyang <lishunyang@mokahr.com>
2019-12-25 01:26:37 +0800

    Release version v1.0.0

---
+++ UnusedWebpackPlugin.js
@@ -0,0 +1,66 @@
+"use strict";
+var fs = require("fs");
+var glob = require("glob");
+var UnusedWebpackPlugin = (function () {
+    function UnusedWebpackPlugin(options) {
+        this.options = options || { cwd: '', patterns: [], ignores: [], output: '' };
+        this.options.cwd = options.cwd || './';
+        this.options.patterns = options.patterns || ['**/*.js', '**/*.styl'];
+        this.options.ignores = (options.ignores || []).concat('node_modules/**');
+        this.options.output = options.output || './unused-files';
+        this.apply = this.apply.bind(this);
+    }
+    UnusedWebpackPlugin.prototype.apply = function (compiler) {
+        var _this = this;
+        compiler.plugin('after-compile', function (compilation, callback) {
+            var localDependencies = new Set();
+            for (var _i = 0, _a = compilation.fileDependencies; _i < _a.length; _i++) {
+                var dependency = _a[_i];
+                if (!/node_modules/.test(dependency)) {
+                    localDependencies.add(dependency);
+                }
+            }
+            Promise.all(_this.options.patterns.map(function (pattern) {
+                return new Promise(function (resolve, reject) {
+                    glob(pattern, { cwd: _this.options.cwd, ignore: _this.options.ignores, absolute: true }, function (err, files) {
+                        if (err) {
+                            reject(err);
+                        }
+                        else {
+                            resolve(files);
+                        }
+                    });
+                });
+            }))
+                .then(function (matches) {
+                var unusedFiles = [];
+                for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
+                    var files = matches_1[_i];
+                    for (var _a = 0, files_1 = files; _a < files_1.length; _a++) {
+                        var file = files_1[_a];
+                        if (!localDependencies.has(file)) {
+                            unusedFiles.push(file);
+                        }
+                    }
+                }
+                return new Promise(function (resolve, reject) {
+                    fs.writeFile(_this.options.output, unusedFiles.join('\n'), function (err) {
+                        if (err) {
+                            reject(err);
+                        }
+                        else {
+                            resolve();
+                        }
+                    });
+                });
+            })
+                .then(function () {
+                callback();
+            })["catch"](function (err) {
+                callback(err);
+            });
+        });
+    };
+    return UnusedWebpackPlugin;
+}());
+module.exports = UnusedWebpackPlugin;
